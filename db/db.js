667325a06fa5878c65dd1bcf58c12e99f47416ea
lwhay <lwh@whu.edu.cn>
2019-10-23 10:53:31 +0800

    use local mysql password.

--- db/db.js
+++ db/db.js
@@ -11,7 +11,7 @@ const pool = mysql.createPool({
     connectionLimit: 10,
     host: 'localhost',
     user: 'root',
-    password: 'Yandai#2018!',
+    password: 'lwh791209',
     database: 'pointcloud'
 });
 
--- server.js
+++ server.js
@@ -94,7 +94,7 @@ app.get("/get_dat_arr", function (req, res) {
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Yandai#2018!',
+    password: 'lwh791209',
     database: 'pointcloud'
 });
 connection.connect();
